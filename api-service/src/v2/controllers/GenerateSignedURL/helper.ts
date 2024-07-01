e5e52ae76684ec910fb946ab402ecd2a7e5ddbb7
yashashk <yashashk@sanketika.in>
2024-07-01 16:42:22 +0530

    Sanketika-obsrv/issue-tracker: #OBS-150: removed lodash map

--- api-service/src/v2/controllers/GenerateSignedURL/helper.ts
+++ api-service/src/v2/controllers/GenerateSignedURL/helper.ts
@@ -35,20 +35,20 @@ const transformFileNames = (fileList: Array<string | any>, access: string, conta
         return transformReadFiles(fileList, containerType);
     }
     return transformWriteFiles(fileList, containerType);
-}
+};
 
 const transformReadFiles = (fileNames: Array<string | any>, containerType: string) => {
     const fileMap = new Map();
-    const updatedFileNames = _.map(fileNames, file => {
+    const updatedFileNames = fileNames.map(file => {
         fileMap.set(file, file);
         return getFilePath(file, containerType);
     });
     return { filesList: fileMap, updatedFileNames };
-}
+};
 
 const transformWriteFiles = (fileNames: Array<string | any>, containerType: string) => {
     const fileMap = new Map();
-    const updatedFileNames = _.map(fileNames, file => {
+    const updatedFileNames = fileNames.map(file => {
         const uuid = uuidv4().replace(/-/g, "").slice(0, 6);
         const ext = path.extname(file);
         const baseName = path.basename(file, ext);
@@ -57,7 +57,7 @@ const transformWriteFiles = (fileNames: Array<string | any>, containerType: stri
         return getFilePath(updatedFileName, containerType);
     });
     return { filesList: fileMap, updatedFileNames };
-}
+};
 
 const getURLExpiry = (access: string) => {
     return access === URLAccess.Read ? config.presigned_url_configs.read_storage_url_expiry : config.presigned_url_configs.write_storage_url_expiry;
