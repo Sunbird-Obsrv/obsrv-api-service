c8ef5e15d1a02df5515420a9430e6078d61f1721
SurabhiAngadi <surabhia@sanketika.in>
2024-07-22 16:47:17 +0530

    #OBS-I21: feat: dataset publish changes for connectors

--- command-service/src/command/connector_command.py
+++ command-service/src/command/connector_command.py
@@ -1,9 +1,12 @@
+from dacite import from_dict
+import json
 import logging
 import subprocess
 
 from command.icommand import ICommand
 from config import Config
 from model.data_models import Action, ActionResponse, CommandPayload, DatasetStatusType
+from model.db_models import ConnectorInstance
 from service.db_service import DatabaseService
 
 
@@ -12,10 +15,7 @@ class ConnectorCommand(ICommand):
         self.config = config
         self.db_service = db_service
         self.logger = logging.getLogger()
-        self.connector_job_ns = "connector-jobs"
-        self.connector_job_chart_dir = "{0}/connector-cron-jobs".format(
-            self.config.find("helm_charts_base_dir")
-        )
+        self.connector_job_config = self.config.find("connector_jobs")
 
     def execute(self, command_payload: CommandPayload, action: Action):
         result = None
@@ -25,6 +25,10 @@ class ConnectorCommand(ICommand):
         is_masterdata = self._get_masterdata_details(
             dataset_id=command_payload.dataset_id
         )
+
+        print(f"Active connectors: {active_connectors}")
+        print(f"Is masterdata: {is_masterdata}")
+
         if action == Action.DEPLOY_CONNECTORS.name:
             result = self._deploy_connectors(
                 command_payload.dataset_id, active_connectors, is_masterdata
@@ -34,7 +38,7 @@ class ConnectorCommand(ICommand):
 
     def _deploy_connectors(self, dataset_id, active_connectors, is_masterdata):
         result = None
-        self._stop_connector_jobs(dataset_id, active_connectors, is_masterdata)
+        # self._stop_connector_jobs(dataset_id, active_connectors, is_masterdata)
         result = self._install_jobs(dataset_id, active_connectors, is_masterdata)
 
         return result
@@ -82,42 +86,59 @@ class ConnectorCommand(ICommand):
 
     def _install_jobs(self, dataset_id, active_connectors, is_masterdata):
         result = None
+
         for connector in active_connectors:
             print("Installing connector {0}".format(connector))
-            connector_jar_config = self.config.find("connector_job")[connector]
-            for release in connector_jar_config:
-                result = self._perform_install(release)
-        if is_masterdata:
-            print("Installing masterdata job")
-            masterdata_jar_config = self.config.find("masterdata_job")
-            for release in masterdata_jar_config:
-                result = self._perform_install(release)
+
+            if connector.connector_runtime == "spark":
+                result = self._perform_spark_install(connector)
+            else:
+                print(
+                    f"Connector {connector.connector_id} is not supported for deployment"
+                )
+                break
+
+        # if is_masterdata:
+        #     print("Installing masterdata job")
+        #     masterdata_jar_config = self.config.find("masterdata_job")
+        #     for release in masterdata_jar_config:
+        #         result = self._perform_install(release)
         return result
 
-    def _perform_install(self, release):
+    def _perform_spark_install(self, connector_instance):
         err = None
         result = None
-        release_name = release["release_name"]
+        release_name = connector_instance.id
+        # print("Instance -->>", connector_instance)
+        connector_source = json.loads(connector_instance.connector_source)
+        print(connector_source)
         helm_install_cmd = [
             "helm",
             "upgrade",
             "--install",
             release_name,
-            self.connector_job_chart_dir,
+            f"""{self.config.find("helm_charts_base_dir")}/{self.connector_job_config["spark"]["base_helm_chart"]}""",
             "--namespace",
-            self.connector_job_ns,
+            self.connector_job_config["spark"]["namespace"],
             "--create-namespace",
             "--set",
-            "file.path={}".format(release["jar"]),
+            "technology={}".format(connector_instance.technology),
             "--set",
-            "class.name={}".format(release["class"]),
+            "instance_id={}".format(release_name),
             "--set",
-            "job.name={}".format(release_name),
+            "connector_source={}".format(connector_source["source"]),
             "--set",
-            "args={}".format(",".join(release["args"])),
+            "main_class={}".format(connector_source["main_class"]),
             "--set",
-            "schedule={}".format(release["schedule"]),
+            "main_file={}".format(connector_source["main_program"]),
+            "--set",
+            "serviceAccount.create=false",
+            "--set",
+            "cronSchedule={}".format(connector_instance.operations_config["schedule"])
         ]
+
+        print(" ".join(helm_install_cmd))
+
         helm_install_result = subprocess.run(
             helm_install_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE
         )
@@ -128,7 +149,7 @@ class ConnectorCommand(ICommand):
             result = ActionResponse(
                 status="ERROR",
                 status_code=500,
-                error_message="FLINK_HELM_INSTALLATION_EXCEPTION",
+                error_message="SPARK_CRON_HELM_INSTALLATION_EXCEPTION",
             )
             print(
                 f"Error re-installing job {release_name}: {helm_install_result.stderr.decode()}"
@@ -141,15 +162,19 @@ class ConnectorCommand(ICommand):
 
     def _get_connector_details(self, dataset_id):
         active_connectors = []
-        rows = self.db_service.execute_select_all(
+        records = self.db_service.execute_select_all(
             f"""
-                SELECT DISTINCT(connector_type)
-                FROM dataset_source_config
-                WHERE status='{DatasetStatusType.Live.name}' and connector_type IN ('object', 'jdbc')"""
+                SELECT ci.id, ci.connector_id, ci.operations_config, cr.runtime as connector_runtime, cr.source as connector_source, cr.technology
+                FROM connector_instances ci
+                JOIN connector_registry cr on ci.connector_id = cr.id
+                WHERE ci.status='{DatasetStatusType.Live.name}' and ci.dataset_id = '{dataset_id}'
+            """
         )
 
-        for row in rows:
-            active_connectors.append(row[0])
+        for record in records:
+            active_connectors.append(from_dict(
+                data_class=ConnectorInstance, data=record
+            ))
 
         return active_connectors
 
@@ -159,7 +184,8 @@ class ConnectorCommand(ICommand):
             f"""
                 SELECT *
                 FROM datasets
-                WHERE status='{DatasetStatusType.Live.name}' AND dataset_id = '{dataset_id}' AND type = 'master-dataset'"""
+                WHERE status='{DatasetStatusType.Live.name}' AND dataset_id = '{dataset_id}' AND type = 'master'
+            """
         )
 
         if len(rows) > 0:
