ec4a1b8aae7efcf79d2b4051a9465c556e9973ce
Rakshitha-D <rakshithagowda.d@gmail.com>
2024-07-30 14:30:13 +0530

    #OBS-I142: formatted connector list api files

--- api-service/src/controllers/ConnectorsList/ConnectorsList.ts
+++ api-service/src/controllers/ConnectorsList/ConnectorsList.ts
@@ -8,9 +8,9 @@ import { ResponseHandler } from "../../helpers/ResponseHandler";
 import httpStatus from "http-status";
 import { connectorService } from "../../services/ConnectorService";
 
-const defaultFeilds = ["id","connector_id","name", "type", "category", "version", "description", "technology", "runtime", "licence", "owner", "iconurl", "status", "created_by", "updated_by", "created_date", "updated_date"];
+const defaultFeilds = ["id", "connector_id", "name", "type", "category", "version", "description", "technology", "runtime", "licence", "owner", "iconurl", "status", "created_by", "updated_by", "created_date", "updated_date"];
 
-const validateRequest = (req: Request)=>{
+const validateRequest = (req: Request) => {
     const isRequestValid: Record<string, any> = schemaValidation(req.body, ConnectorListSchema)
     if (!isRequestValid.isValid) {
         throw obsrvError("", "CONNECTORS_LIST_INPUT_INVALID", isRequestValid.message, "BAD_REQUEST", 400)
@@ -19,28 +19,28 @@ const validateRequest = (req: Request)=>{
 
 const connectorsList = async (req: Request, res: Response) => {
     validateRequest(req);
-     
+
     const connectorBody = req.body.request;
     const connectorList = await listConnectors(connectorBody)
     const responseData = { data: connectorList, count: _.size(connectorList) }
-    logger.info({req: req.body, resmsgid: _.get(res, "resmsgid"), message: `Connectors are listed successfully with a connectors count (${_.size(connectorList)})` })
+    logger.info({ req: req.body, resmsgid: _.get(res, "resmsgid"), message: `Connectors are listed successfully with a connectors count (${_.size(connectorList)})` })
     ResponseHandler.successResponse(req, res, { status: httpStatus.OK, data: responseData });
 }
 
 const listConnectors = async (request: Record<string, any>): Promise<Record<string, any>> => {
     const { filters = {} } = request || {};
     const connectorStatus = _.get(filters, "status");
-    const connectorCategory = _.get(filters,"category");
-    const filterOptions: any  = {};
-    if(!_.isEmpty(connectorStatus)){
+    const connectorCategory = _.get(filters, "category");
+    const filterOptions: any = {};
+    if (!_.isEmpty(connectorStatus)) {
         filterOptions["status"] = connectorStatus
     }
 
-    if(!_.isEmpty(connectorCategory)){
-        filterOptions["category"] =connectorCategory
+    if (!_.isEmpty(connectorCategory)) {
+        filterOptions["category"] = connectorCategory
     }
-    const filteredconnectorList = await connectorService.findConnectors(filterOptions,defaultFeilds);
-    return filteredconnectorList ;    
+    const filteredconnectorList = await connectorService.findConnectors(filterOptions, defaultFeilds);
+    return filteredconnectorList;
 }
 
 export default connectorsList;
--- api-service/src/services/ConnectorService.ts
+++ api-service/src/services/ConnectorService.ts
@@ -1,11 +1,11 @@
 import { ConnectorRegistry } from "../models/ConnectorRegistry";
 
-class ConnectorService{
+class ConnectorService {
 
     findConnectors = async (where?: Record<string, any>, attributes?: string[]): Promise<any> => {
-        return ConnectorRegistry.findAll({where, attributes, raw: true});
+        return ConnectorRegistry.findAll({ where, attributes, raw: true });
     }
-  
+
 }
 
 export const connectorService = new ConnectorService();
