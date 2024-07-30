dba36d02c856fa0789dedd6d80ccdf4605e9f339
Rakshitha-D <rakshithagowda.d@gmail.com>
2024-07-30 11:21:51 +0530

    connector list

---
+++ api-service/src/services/ConnectorService.ts
@@ -0,0 +1,11 @@
+import { ConnectorRegistry } from "../models/ConnectorRegistry";
+
+class ConnectorService{
+
+    findConnectors = async (where?: Record<string, any>, attributes?: string[]): Promise<any> => {
+        return ConnectorRegistry.findAll({where, attributes, raw: true});
+    }
+  
+}
+
+export const connectorService = new ConnectorService();
