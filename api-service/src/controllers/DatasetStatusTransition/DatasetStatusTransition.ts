f0e2e2b0dba6c0bfd9ca40a54fec7cfc6beb1e67
yashashk <yashashk@sanketika.in>
2024-07-31 18:17:05 +0530

    #OBS-I138: merging dataset defaults to dataset draft record before saving

--- api-service/src/controllers/DatasetStatusTransition/DatasetStatusTransition.ts
+++ api-service/src/controllers/DatasetStatusTransition/DatasetStatusTransition.ts
@@ -95,10 +95,11 @@ const readyForPublish = async (dataset: Record<string, any>) => {
     let draftDataset: any = await datasetService.getDraftDataset(dataset.dataset_id)
     let defaultConfigs: any = _.cloneDeep(defaultDatasetConfig)
     defaultConfigs = _.omit(defaultConfigs, ["router_config"])
-    if (draftDataset?.type === 'master') {
-        defaultConfigs = _.omit(defaultConfigs, "dataset_config.keys_config.data_key");
-    }
-    _.merge(draftDataset, defaultConfigs)
+    _.mergeWith(draftDataset, defaultConfigs, draftDataset, (objValue, srcValue) => {
+        if (_.isBoolean(objValue) && _.isBoolean(srcValue)) {
+            return objValue;
+        }
+    });
     const datasetValid: Record<string, any> = schemaValidation(draftDataset, ReadyToPublishSchema)
     if (!datasetValid.isValid) {
         throw {
