63288845afe22bb6a52adff36a88ca0ebb49f7c2
Rakshitha-D <rakshithagowda.d@gmail.com>
2024-08-01 15:38:49 +0530

    #OBS-I145: updated the connector read api

--- api-service/src/controllers/ConnectorsRead/ConnectorsRead.ts
+++ api-service/src/controllers/ConnectorsRead/ConnectorsRead.ts
@@ -3,19 +3,20 @@ import { obsrvError } from "../../types/ObsrvError";
 import { ResponseHandler } from "../../helpers/ResponseHandler";
 import httpStatus from "http-status";
 import { connectorService } from "../../services/ConnectorService";
+import _ from "lodash";
 
 const defaultFields = ["id", "connector_id", "name", "type", "category", "version", "description", "licence", "owner", "iconurl", "status", "ui_spec", "created_by", "updated_by", "created_date", "updated_date", "live_date"];
 
 const connectorsRead = async (req: Request, res: Response) => {
     const { id } = req.params;
     const { mode } = req.query;
-    
-    const connector = (mode?.toString()?.toLowerCase()) === "edit" ? await connectorService.getConnector({ id: id, status: "Draft" }, defaultFields) : await connectorService.getConnector({ id: id, status: "Live" }, defaultFields);
+    const isEditMode = _.toLower(_.toString(mode)) === "edit"
+    const status = isEditMode ? "Draft" : "Live"
+    const connector = await connectorService.getConnector({ id, status }, defaultFields)
     if (!connector) {
-        throw obsrvError(id, "CONNECTOR_NOT_FOUND", `Connector not found: ${id}`, "NOT_FOUND", 404);
-    } else {
-        ResponseHandler.successResponse(req, res, { status: httpStatus.OK, data: connector });
+        throw obsrvError("", "CONNECTOR_NOT_FOUND", `Connector not found: ${id}`, "NOT_FOUND", 404);
     }
+    ResponseHandler.successResponse(req, res, { status: httpStatus.OK, data: connector });
 }
 
 export default connectorsRead;
