ce5ccff713b09e1e0549165ceb1f1977e8fc03b1
yashashk <yashashk@sanketika.in>
2024-08-20 12:55:51 +0530

    #OBS-I167 : read api changes while reading connectors according to v2 structure

--- api-service/src/controllers/DatasetRead/DatasetRead.ts
+++ api-service/src/controllers/DatasetRead/DatasetRead.ts
@@ -39,36 +39,70 @@ const datasetRead = async (req: Request, res: Response) => {
         throw obsrvError(dataset_id, "DATASET_NOT_FOUND", `Dataset with the given dataset_id:${dataset_id} not found`, "NOT_FOUND", 404);
     }
     if (dataset.connectors_config) {
-        dataset.connectors_config = dataset.connectors_config.map((connector: any) => ({
-            ...connector,
-            connector_config: JSON.parse(cipherService.decrypt(connector.connector_config))
-        }));
+        dataset.connectors_config = dataset?.connectors_config.map((connector: any) => {
+            const connector_config = _.get(connector, "connector_config")
+            return {
+                ...connector,
+                connector_config: _.isObject(connector_config) ? connector_config : JSON.parse(cipherService.decrypt(connector.connector_config))
+            }
+        });
     }
     ResponseHandler.successResponse(req, res, { status: httpStatus.OK, data: dataset });
 }
 
 const readDraftDataset = async (datasetId: string, attributes: string[]): Promise<any> => {
-    
+
     const attrs = _.union(attributes, ["dataset_config", "api_version", "type", "id"])
     const draftDataset = await datasetService.getDraftDataset(datasetId, attrs);
-    if(draftDataset) { // Contains a draft
+    if (draftDataset) { // Contains a draft
         const apiVersion = _.get(draftDataset, ["api_version"]);
         const dataset: any = (apiVersion === "v2") ? draftDataset : await datasetService.migrateDraftDataset(datasetId, draftDataset)
-        return _.pick(dataset, attributes); 
+        return _.pick(dataset, attributes);
     }
 
     const liveDataset = await datasetService.getDataset(datasetId, undefined, true);
-    if(liveDataset) {
+    if (liveDataset) {
         const dataset = await datasetService.createDraftDatasetFromLive(liveDataset)
-        return _.pick(dataset, attributes); 
+        return _.pick(dataset, attributes);
     }
-    
+
     return null;
 }
 
 const readDataset = async (datasetId: string, attributes: string[]): Promise<any> => {
     const dataset = await datasetService.getDataset(datasetId, attributes, true);
-    return dataset;
+    const api_version = _.get(dataset, "api_version")
+    let datasetConfigs: any = {}
+    const connectors_config: any[] = await datasetService.getConnectorsV1(datasetId, ["id", "dataset_id", "connector_config", "connector_type"]);
+    const transformations_config = await datasetService.getTransformations(datasetId, ["field_key", "transformation_function", "mode", "metadata"])
+    if (api_version !== "v2") {
+        datasetConfigs["connectors_config"] = _.map(connectors_config, (config) => {
+            return {
+                id: _.get(config, "id"),
+                connector_id: _.get(config, "connector_type"),
+                connector_config: _.get(config, "connector_config"),
+                version: "v1"
+            }
+        })
+        datasetConfigs["transformations_config"] = _.map(transformations_config, (config) => {
+            console.log(config)
+            const section: any = _.get(config, "metadata.section") || _.get(config, "transformation_function.category");
+            return {
+                field_key: _.get(config, "field_key"),
+                transformation_function: {
+                    ..._.get(config, ["transformation_function"]),
+                    datatype: _.get(config, "metadata._transformedFieldDataType") || _.get(config, "transformation_function.datatype") || "string",
+                    category: datasetService.getTransformationCategory(section),
+                },
+                mode: _.get(config, "mode")
+            }
+        })
+    }
+    else {
+        datasetConfigs["connectors_config"] = connectors_config;
+        datasetConfigs["transformations_config"] = transformations_config;
+    }
+    return { ...dataset, ...datasetConfigs };
 }
 
 export default datasetRead;
