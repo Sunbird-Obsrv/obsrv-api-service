02bd46b7219a73735ea84c0296dc27960721ae2f
Rakshitha-D <rakshithagowda.d@gmail.com>
2024-09-12 10:29:57 +0530

    #OBS-I165: modified the user permissions into a json object

--- api-service/src/middlewares/RBAC_middleware.ts
+++ api-service/src/middlewares/RBAC_middleware.ts
@@ -4,205 +4,144 @@ import { ResponseHandler } from "../helpers/ResponseHandler";
 import { config } from "../configs/Config";
 import _ from "lodash";
 
-enum roles {
-  OperationsAdmin = "operations_admin",
-  Admin = "admin",
-  DatasetManager = "dataset_manager",
-  Viewer = "viewer",
-  DatasetCreator = "dataset_creator",
-  Ingestor = "ingestor",
-}
-
-enum permissions {
-  AlertCreate = "api.alert.create",
-  AlertPublish = "api.alert.publish",
-  AlertUpdate = "api.alert.update",
-  AlertList = "api.alert.list",
-  AlertDelete = "api.alert.delete",
-  AlertDetails = "api.alert.getAlertDetails",
-  MetricCreate = "api.metric.add",
-  MetricList = "api.metric.list",
-  MetricUpdate = "api.metric.update",
-  MetricRemove = "api.metric.remove",
-  SilenceCreate = "api.alert.silence.create",
-  SilenceList = "api.alert.silence.list",
-  SilenceRead = "api.alert.silence.get",
-  SilenceEdit = "api.alert.silence.edit",
-  SilenceDelete = "api.alert.silence.delete",
-  NotificationChannelCreate = "api.alert.notification.create",
-  NotificationChannelList = "api.alert.notification.list",
-  NotificationChannelPublish = "api.alert.notification.publish",
-  NotificationChannelTest = "api.alert.notification.test",
-  NotificationChannelUpdate = "api.alert.notification.update",
-  NotificationChannelRetire = "api.alert.notification.retire",
-  NotificationChannelRead = "api.alert.notification.get",
-  DatasetCreate = "api.datasets.create",
-  DatasetUpdate = "api.datasets.update",
-  DatasetRead = "api.datasets.read",
-  DatasetList = "api.datasets.list",
-  DataIngest = "api.data.in",
-  DataOut = "api.data.out",
-  DataExhaust = "api.data.exhaust",
-  QueryTemplateCreate = "api.query.template.create",
-  QueryTemplateRead = "api.query.template.read",
-  QueryTemplateDelete = "api.query.template.delete",
-  QueryTemplateList = "api.query.template.list",
-  QueryTemplateUpdate = "api.query.template.update",
-  QueryTemplate = "api.query.template.query",
-  SchemaValidator = "api.schema.validator",
-  GenerateSignedUrl = "api.files.generate-url",
-  DatasetStatusTransition = "api.datasets.status-transition",
-  DatasetHealth = "api.dataset.health",
-  DatasetReset = "api.dataset.reset",
-  DatasetSchemaGenerator = "api.datasets.dataschema",
-  DatasetExport = "api.datasets.export",
-  DatasetCopy = "api.datasets.copy",
-  ConnectorList = "api.connectors.list",
-  ConnectorRead = "api.connectors.read",
-  DatasetImport = "api.datasets.import",
-  SQLQuery = "api.obsrv.data.sql-query",
-}
-
 interface AccessControl {
-  [key: string]: string[];
+  apiGroups : {
+    [key: string]: string[];
+  },
+  roles : {
+    [key: string]: string[];
+  }
 }
 
 const accessControl: AccessControl = {
-  [roles.Ingestor]: [permissions.DataIngest],
-  [roles.Viewer]: [
-    permissions.DatasetList,
-    permissions.DatasetRead,
-    permissions.DatasetExport,
-    permissions.ConnectorRead,
-    permissions.SQLQuery,
-    permissions.DataOut,
-    permissions.DataExhaust,
-    permissions.AlertList,
-    permissions.AlertDetails,
-    permissions.MetricList,
-    permissions.SilenceList,
-    permissions.SilenceRead,
-    permissions.NotificationChannelList,
-    permissions.NotificationChannelRead,
-  ],
-  [roles.DatasetCreator]: [
-    permissions.DatasetList,
-    permissions.DatasetRead,
-    permissions.DatasetExport,
-    permissions.ConnectorRead,
-    permissions.SQLQuery,
-    permissions.DataOut,
-    permissions.DataExhaust,
-    permissions.DatasetImport,
-    permissions.DatasetCreate,
-    permissions.DatasetUpdate,
-    permissions.DatasetCopy,
-    permissions.QueryTemplateCreate,
-    permissions.QueryTemplateRead,
-    permissions.QueryTemplateDelete,
-    permissions.QueryTemplateUpdate,
-    permissions.GenerateSignedUrl,
-    permissions.SchemaValidator,
-    permissions.DatasetSchemaGenerator,
-    permissions.AlertList,
-    permissions.AlertDetails,
-    permissions.MetricList,
-    permissions.SilenceList,
-    permissions.SilenceRead,
-    permissions.NotificationChannelList,
-    permissions.NotificationChannelRead,
-  ],
-  [roles.DatasetManager]: [
-    permissions.DatasetList,
-    permissions.DatasetRead,
-    permissions.DatasetExport,
-    permissions.ConnectorRead,
-    permissions.SQLQuery,
-    permissions.DataOut,
-    permissions.DataExhaust,
-    permissions.DatasetImport,
-    permissions.DatasetCreate,
-    permissions.DatasetUpdate,
-    permissions.DatasetCopy,
-    permissions.QueryTemplateCreate,
-    permissions.QueryTemplateRead,
-    permissions.QueryTemplateDelete,
-    permissions.QueryTemplateUpdate,
-    permissions.GenerateSignedUrl,
-    permissions.SchemaValidator,
-    permissions.DatasetSchemaGenerator,
-    permissions.DatasetReset,
-    permissions.DatasetStatusTransition,
-    permissions.AlertList,
-    permissions.AlertDetails,
-    permissions.MetricList,
-    permissions.SilenceList,
-    permissions.SilenceRead,
-    permissions.NotificationChannelList,
-    permissions.NotificationChannelRead,
-  ],
-  [roles.Admin]: [
-    permissions.DatasetCreate,
-    permissions.DatasetList,
-    permissions.DatasetRead,
-    permissions.DatasetExport,
-    permissions.ConnectorRead,
-    permissions.SQLQuery,
-    permissions.DataOut,
-    permissions.DataExhaust,
-    permissions.DatasetImport,
-    permissions.DatasetCreate,
-    permissions.DatasetUpdate,
-    permissions.DatasetCopy,
-    permissions.QueryTemplateCreate,
-    permissions.QueryTemplateRead,
-    permissions.QueryTemplateDelete,
-    permissions.QueryTemplateUpdate,
-    permissions.GenerateSignedUrl,
-    permissions.SchemaValidator,
-    permissions.DatasetSchemaGenerator,
-    permissions.DatasetReset,
-    permissions.DatasetStatusTransition,
-    permissions.AlertList,
-    permissions.AlertDetails,
-    permissions.MetricList,
-    permissions.SilenceList,
-    permissions.SilenceRead,
-    permissions.NotificationChannelList,
-    permissions.NotificationChannelRead,
-  ],
-  [roles.OperationsAdmin]: [
-    permissions.AlertCreate,
-    permissions.AlertPublish,
-    permissions.AlertUpdate,
-    permissions.AlertList,
-    permissions.AlertDelete,
-    permissions.AlertDetails,
-    permissions.MetricCreate,
-    permissions.MetricList,
-    permissions.MetricUpdate,
-    permissions.MetricRemove,
-    permissions.SilenceCreate,
-    permissions.SilenceList,
-    permissions.SilenceRead,
-    permissions.SilenceEdit,
-    permissions.SilenceDelete,
-    permissions.NotificationChannelCreate,
-    permissions.NotificationChannelList,
-    permissions.NotificationChannelPublish,
-    permissions.NotificationChannelTest,
-    permissions.NotificationChannelUpdate,
-    permissions.NotificationChannelRetire,
-    permissions.NotificationChannelRead,
-    permissions.DatasetList,
-    permissions.DatasetRead,
-    permissions.DatasetExport,
-    permissions.ConnectorRead,
-    permissions.SQLQuery,
-    permissions.DataOut,
-    permissions.DataExhaust,
-  ],
-};
+  "apiGroups": {
+      "general_access": [
+          "api.datasets.list",
+          "api.datasets.read",
+          "api.datasets.export",
+          "api.data.out",
+          "api.data.exhaust",
+          "api.alert.list",
+          "api.alert.getAlertDetails",
+          "api.metric.list",
+          "api.alert.silence.list",
+          "api.alert.silence.get",
+          "api.alert.notification.list",
+          "api.alert.notification.get"
+      ],
+      "restricted_dataset_api": [
+          "api.datasets.reset",
+          "api.datasets.status-transition"
+      ],
+      "alert": [
+          "api.alert.create",
+          "api.alert.publish",
+          "api.alert.update",
+          "api.alert.delete"
+      ],
+      "metric": [
+          "api.metric.add",
+          "api.metric.update",
+          "api.metric.remove"
+      ],
+      "silence": [
+          "api.alert.silence.create",
+          "api.alert.silence.edit",
+          "api.alert.silence.delete"
+      ],
+      "notificationChannel": [
+          "api.alert.notification.create",
+          "api.alert.notification.publish",
+          "api.alert.notification.test",
+          "api.alert.notification.update",
+          "api.alert.notification.retire"
+      ],
+      "dataset": [
+          "api.datasets.create",
+          "api.datasets.update",
+          "api.datasets.import",
+          "api.datasets.copy",
+          "api.dataset.health",
+          "api.datasets.dataschema"
+      ],
+      "data": [
+          "api.data.in"
+      ],
+      "queryTemplate": [
+          "api.query.template.create",
+          "api.query.template.read",
+          "api.query.template.delete",
+          "api.query.template.update",
+          "api.query.template.query",
+          "api.query.template.list"
+      ],
+      "schema": [
+          "api.schema.validator"
+      ],
+      "file": [
+          "api.files.generate-url"
+      ],
+      "connector": [
+          "api.connectors.list",
+          "api.connectors.read"
+      ],
+      "sqlQuery": [
+          "api.obsrv.data.sql-query"
+      ]
+  },
+  "roles": {
+      "ingestor": [
+          "data"
+      ],
+      "viewer": [
+          "general_access",
+          "connector",
+          "sqlQuery"
+      ],
+      "dataset_creator": [
+          "general_access",
+          "connector",
+          "sqlQuery",
+          "dataset",
+          "queryTemplate",
+          "schema",
+          "file",
+          "connector",
+          "sqlQuery"
+      ],
+      "dataset_manager": [
+          "general_access",
+          "connector",
+          "sqlQuery",
+          "dataset",
+          "queryTemplate",
+          "schema",
+          "file",
+          "connector",
+          "sqlQuery",
+          "restricted_dataset_api"
+      ],
+      "admin": [
+          "general_access",
+          "connector",
+          "sqlQuery",
+          "dataset",
+          "queryTemplate",
+          "schema",
+          "file",
+          "connector",
+          "sqlQuery",
+          "restricted_dataset_api"
+      ],
+      "operations_admin": [
+          "alert",
+          "metric",
+          "silence",
+          "notificationChannel",
+          "general_access"
+      ]
+  }
+}
 
 export default {
   name: "rbac:middleware",
@@ -239,10 +178,19 @@ export default {
           if (decoded && _.isObject(decoded)) {
             (req as any).userInfo = decoded;
             const action = (req as any).id;
-            const hasAccess = decoded?.roles?.some(
-              (role: string) =>
-                accessControl[role] && accessControl[role].includes(action)
-            );
+            // const hasAccess = decoded?.roles?.some(
+            //   (role: string) =>
+            //     accessControl[role] && accessControl[role].includes(action)
+            // );
+            const hasAccess = decoded?.roles?.some((role: string) => {
+              const apiGroups = accessControl.roles[role];
+
+              if (!apiGroups) return false;
+
+              return apiGroups.some((apiGroup: string) =>
+                accessControl.apiGroups[apiGroup]?.includes(action)
+              );
+            });
             if (!hasAccess) {
               return ResponseHandler.errorResponse(
                 {
